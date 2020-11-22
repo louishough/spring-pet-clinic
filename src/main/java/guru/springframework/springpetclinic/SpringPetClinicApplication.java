8ef92e37a1c78517f46b4d59b40f26d423db20b5
Louis <louishough1998@gmail.com>
2020-11-22 19:24:10 +0000

    update gitignore

--- src/main/java/guru/springframework/springpetclinic/SpringPetClinicApplication.java
+++ src/main/java/guru/springframework/springpetclinic/SpringPetClinicApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringPetclinicApplication {
+public class SpringPetClinicApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringPetclinicApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(SpringPetClinicApplication.class, args);
+  }
 }
